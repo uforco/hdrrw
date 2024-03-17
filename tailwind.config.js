39c868bb5ba7b151742033b3c809959d9e5ccc3d
Sharif <srka780@gmail.com>
2024-03-17 22:35:45 +0600

    netlify upload project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
 // export default {
 //   content: [
 //     "./index.html",
@@ -9,12 +10,13 @@
 //   },
 //   plugins: [require("daisyui")],
 // }
-const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
-  content: [
+   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
